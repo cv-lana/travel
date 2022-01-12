64a16b4c0ad89be0e235d315b3522931e0cd99db
Svetlana <sl1248@yandex.ru>
2022-01-12 19:06:34 +0300

    added responsive for gallery section

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -5,12 +5,28 @@ const slider = () => {
   const swiper = new Swiper('.swiper', {
     loop: true,
     slidesPerColumnFill: 'column',
-    slidesPerView: 3,
-    spaceBetween: 32,
     navigation: {
       nextEl: '.gallery__btn--next',
       prevEl: '.gallery__btn--prev',
     },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      577: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      993: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1201: {
+        slidesPerView: 3,
+        spaceBetween: 32,
+      }
+    }
   });
 };
 
