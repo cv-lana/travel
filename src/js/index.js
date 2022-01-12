24ea6e467d4620b56a7f438d5c56830cf931e0e4
Svetlana <sl1248@yandex.ru>
2022-01-12 17:19:41 +0300

    added responsive for aboutBaikal section

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,14 @@
 import preload from './modules/preload.js';
 import toggleModal from './modules/toggleModal.js';
 import toggleMenu from './modules/toggleMenu.js';
-import playVideo from './modules/playVideo.js';
 import slider from './modules/slider.js';
 import { showBlockTours, showBlockDirections } from './modules/functions.js';
+import Fancybox from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox.css";
 
 preload();
 toggleModal();
 toggleMenu();
-playVideo();
 slider();
 showBlockTours();
 showBlockDirections();
