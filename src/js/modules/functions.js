1ec5b48cb58e99eca3484a6394d9f6a80fe18fa3
Svetlana <sl1248@yandex.ru>
2022-01-25 23:30:43 +0300

    fixed modal section

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,19 +1,25 @@
 export function disableScroll() {
+  const header = document.querySelector('.header');
   const widthScroll = window.innerWidth - document.body.offsetWidth;
   document.body.dbScrollY = window.scrollY;
   document.body.style.cssText = `
     position: fixed;
     top: ${-window.scrollY}px;
     left: 0;
-    width: 100%
+    width: 100%;
     height: 100vh;
     overflow: hidden;
     padding-right: ${widthScroll}px;
   `;
+  header.style.cssText = `
+    right: ${widthScroll}px;
+  `;
 }
 
 export function enableScroll() {
+  const header = document.querySelector('.header');
   document.body.style.cssText = '';
+  header.style.cssText = '';
   window.scroll({
     top: document.body.dbScrollY,
   });
