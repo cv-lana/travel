72e931cdb059f5ca675de7b160e1f4721271fdb8
Svetlana <sl1248@yandex.ru>
2022-03-06 01:11:44 +0300

    added business-tourism, cruises, summer & winter.html

--- src/js/modules/cardHorizontal.js
+++ src/js/modules/cardHorizontal.js
@@ -4,20 +4,22 @@ class CardHorizontal {
     div.classList.add('vw-card');
 
     div.innerHTML = `
-      <img class="vw-card__image" src="${data.image}" alt="${data.name}">
+      <img class="vw-card__image" src="${data.image}" alt="${data.name}" width="352" height="352">
       <div class="vw-card__content">
         <h3 class="vw-card__title">${data.name}</h3>
         <p class="vw-card__text">${data.description}</p>
         ${data.details ? `<p class="vw-card__text">${data.details}</p>` : ''}
-        ${data.duration ?
+        ${data.duration || data.season ?
         `<div class="vw-card__details">
             <div class="vw-card__details-wrapper">
+            ${data.duration ? `
               <p class="vw-card__details-duration">
                 <span class="vw-card__details-text">Продолжительность:</span> ${data.duration}
-              </p>
+              </p>` : ''}
+            ${data.season ? `
               <p class="vw-card__details-season">
                 <span class="vw-card__details-text">Время проведения:</span> ${data.season}
-              </p>
+              </p>` : ''}
               <p class="vw-card__details-price">
                 <span class="vw-card__details-text">Стоимость:</span> от ${data.price} руб. с человека
               </p>
--- src/js/modules/cardSquare.js
+++ src/js/modules/cardSquare.js
@@ -5,7 +5,7 @@ class CardSquare {
 
     div.innerHTML = `
       <a class="sq-card__link" href="${data.url}">
-        <img class="sq-card__image" src="${data.image}" alt="${data.name}">
+        <img class="sq-card__image" src="${data.image}" alt="${data.name}" width="544" height="352">
         <span class="sq-card__text">${data.name}</span>
         ${data.price ? `<span class="sq-card__price">1 час по городу – ${data.price} руб.</span>` : ''}
       </a>
